10e6787be1655c797ddb09e9a297c12a7774a31a
Ahmed Hamed Ahmed <ahmed.elsunhoty@outlook.com>
2022-05-12 00:18:21 +0300

    - Fix Permission denied on actions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -6,6 +6,7 @@ object Versions {
     const val appcompat = "1.4.1"
     const val materialDesign = "1.5.0"
     const val constraintLayout = "2.1.3"
+
     //test
     const val testCore = "1.4.0"
     const val testArcCore = "2.0.0"
@@ -13,9 +14,11 @@ object Versions {
     const val extJunit = "1.1.3"
     const val espresso = "3.4.0"
     const val robolectric = "4.8"
+
     // room
     const val roomDatabase = "2.4.2"
     const val liveData = "2.4.1"
+
     // restful api
     const val retrofit = "2.9.0"
     const val okHttp = "4.9.1"
